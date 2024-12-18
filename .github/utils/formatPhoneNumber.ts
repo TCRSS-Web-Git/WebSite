2ae6adc7ed981a7a748bf7df7d9b8a9b6b6ca231
Suradech-Wec <suradech_wec@tcrss.com>
2024-12-18 15:10:36 +0700

    Delete .github/utils directory

--- .github/utils/formatPhoneNumber.ts
+++
@@ -1,7 +0,0 @@
-export default function (phoneNumber: string): string {
-  if (phoneNumber?.startsWith('+66')) {
-    // Remove the country code
-    phoneNumber = '0' + phoneNumber.substring(3)
-  }
-  return phoneNumber
-}
