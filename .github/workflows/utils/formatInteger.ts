ebe80d703f1f1d742f49be9073f497f65af91d5d
Suradech-Wec <suradech_wec@tcrss.com>
2024-12-18 14:43:15 +0700

    Delete .github/workflows/utils directory

--- .github/workflows/utils/formatInteger.ts
+++
@@ -1,7 +0,0 @@
-export default function (number: number | null | undefined): string {
-  if (number === null || number === undefined) {
-    return '0'
-  }
-
-  return number.toLocaleString('en-US', { maximumFractionDigits: 0, minimumFractionDigits: 0 })
-}
